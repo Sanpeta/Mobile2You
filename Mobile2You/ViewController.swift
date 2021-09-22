0a5919c0831252becfc6f2430a668dfc0805778a
Sanpeta <sidneisonic@hotmail.com>
2021-09-22 11:48:35 -0300

    Initial Commit

---
+++ Mobile2You/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile2You
+//
+//  Created by Sidnei de Souza Junior on 22/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
