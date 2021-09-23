255db06ce4ceda289b28b75668cec112283b748b
Sanpeta <sidneisonic@hotmail.com>
2021-09-23 12:31:27 -0300

    [MovieTableViewCell] Ajustado o tamanho da imagem, fonte e alinhamento.

--- Mobile2You/View/MovieTableViewCell.swift
+++ Mobile2You/View/MovieTableViewCell.swift
@@ -16,42 +16,59 @@ class MovieTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.backgroundColor = UIColor(named: Constants.backgroundColor)
+        self.frame.size.width = widthScreen
+        self.frame.size.height = heightScreen * 0.11
+        
         imageCell = UIImageView(
             frame: CGRect(
                 x: 8,
-                y: 16,
-                width: self.frame.size.width/10,
-                height: self.frame.size.height - 16
+                y: 8,
+                width: self.frame.width * 0.15,
+                height: self.frame.height - 16
             )
         )
-        imageCell.contentMode = .scaleToFill
+        imageCell.contentMode = .scaleAspectFill
+        
         
         titleCell = UILabel(
             frame: CGRect(
                 x: imageCell.frame.maxX + 8,
-                y: 16,
-                width: self.frame.size.width/10,
-                height: self.frame.size.height - 16
+                y: self.frame.size.height * 0.30,
+                width: self.frame.size.width * 0.70,
+                height: self.frame.size.height * 0.20
             )
         )
+        titleCell.textColor = .white
+        titleCell.textAlignment = .left
+        titleCell.font = UIFont.boldSystemFont(ofSize: 18)
+        
         
         yearCell = UILabel(
             frame: CGRect(
                 x: imageCell.frame.maxX + 8,
-                y: 16,
-                width: self.frame.size.width/10,
-                height: self.frame.size.height - 16
+                y: titleCell.frame.maxY + 8,
+                width: self.frame.size.width * 0.10,
+                height: self.frame.size.height * 0.20
             )
         )
+        yearCell.textColor = .white
+        yearCell.textAlignment = .left
+        yearCell.font = yearCell.font.withSize(14)
+        
         
         genreCell = UILabel(
             frame: CGRect(
-                x: yearCell.frame.maxX + 8,
-                y: 16,
-                width: self.frame.size.width/10,
-                height: self.frame.size.height - 16
+                x: yearCell.frame.maxX,
+                y: titleCell.frame.maxY + 8,
+                width: self.frame.size.width * 0.60,
+                height: self.frame.size.height * 0.20
             )
         )
+        genreCell.textColor = .white
+        genreCell.textAlignment = .left
+        genreCell.font = genreCell.font.withSize(14)
         
         self.contentView.addSubview(imageCell)
         self.contentView.addSubview(titleCell)
