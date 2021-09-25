46e82bff7c7ab0026dd329234d3f12cfabab8c18
Sanpeta <sidneisonic@hotmail.com>
2021-09-24 22:44:11 -0300

    [Service] Criado para consumir API e criado a função searchMovie.

--- Mobile2You/Service/Service.swift
+++ Mobile2You/Service/Service.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class Service: NSObject {
+    static let shared = Service()
+    
+    func searchMovie(completion: @escaping (Movie?, Error?) -> ()) {
+        guard let minhaUrl = NSURL(string: "https://api.themoviedb.org/3/movie/157336?api_key=\(API_KEY)") else {
+            return
+        }
+        let request = NSMutableURLRequest(url: minhaUrl as URL)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            let httpStatus = response as? HTTPURLResponse
+            
+            if httpStatus?.statusCode == 200 {
+                
+                if data?.count != 0 {
+                    
+                    do {
+                        
+                        let movieJSON = try JSONDecoder().decode(Movie.self, from: data!)
+                        
+                        DispatchQueue.main.async {
+                            completion(movieJSON, nil)                            
+                        }
+                        
+                        
+                    } catch let error {
+                        
+                        print("Error to decode \(error)")
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        task.resume()
+    }
+    
+}
