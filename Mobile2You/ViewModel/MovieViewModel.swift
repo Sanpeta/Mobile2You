fa82be6b64e1dca43d42803be39a4de07292658b
Sanpeta <sidneisonic@hotmail.com>
2021-09-24 22:49:49 -0300

    [MovieViewModel] Feito a Dependency Injection e convertido todas variaveis em String.

--- Mobile2You/ViewModel/MovieViewModel.swift
+++ Mobile2You/ViewModel/MovieViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+struct MovieViewModel {    
+    let title: String
+    let vote_count: String
+    let popularity: String
+    let backdrop_path: String
+    
+    //Dependency Injection
+    init(movie: Movie) {
+        self.title = movie.title
+        if movie.vote_count > 1000 {
+            let convertToK = Double(movie.vote_count / 1000)
+            self.vote_count = "\(String(format: "%.1f", convertToK))K Likes"
+        } else {
+            self.vote_count = "\(movie.vote_count) Likes"
+        }
+        self.popularity = "\(movie.popularity) views"
+        self.backdrop_path = movie.backdrop_path
+    }
+    
+}
