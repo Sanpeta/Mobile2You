4f9dd41cb2604bdf63c91c4b41d425f4e836d3d0
Sanpeta <sidneisonic@hotmail.com>
2021-09-25 01:02:15 -0300

    [SimilarMovie] Criado Model SimilarMovie.

--- Mobile2You/Model/SimilarMovie.swift
+++ Mobile2You/Model/SimilarMovie.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Similar: Codable {
+    let page: Int
+    let results: [SimilarMovie]
+}
+
+struct SimilarMovie: Codable {
+    let title: String
+    let poster_path: String?
+    let release_date: String
+    let genre_ids: [Int]
+}
